71643ca2b2759807c5686099d615399815c85b66
Flávia Amaral <flavia.amaral@indexis.com.br>
2016-06-21 23:27:11 -0300

    end lesson 2

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -296,7 +296,6 @@ public  class ForecastFragment extends Fragment {
                                 // This will only happen if there was an error getting or parsing the forecast.
             return null;
         }
-
                         @Override
                 protected void onPostExecute(String[] result) {
                         if (result != null) {
