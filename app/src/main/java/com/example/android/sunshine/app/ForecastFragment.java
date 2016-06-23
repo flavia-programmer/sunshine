ce77a09ea7c839a80ebd0ce5f9fd3cc26ec7359d
Flávia Amaral <flavia.amaral@indexis.com.br>
2016-06-23 00:03:47 -0300

    add code -  20/30 - lesson 3

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -31,8 +31,6 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public  class ForecastFragment extends Fragment {
     private ArrayAdapter<String> mForecastAdapter;
@@ -58,11 +56,7 @@ public  class ForecastFragment extends Fragment {
             // as you specify a parent activity in AndroidManifest.xml.
                 int id = item.getItemId();
                 if (id == R.id.action_refresh) {
-                    FetchWeatherTask weatherTask = new FetchWeatherTask();
-                    SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
-                    String location = prefs.getString(getString(R.string.pref_location_key),
-                    getString(R.string.pref_location_default));
-                    weatherTask.execute(location);
+                    updateWeather();
                     return true;
                 }
                 return super.onOptionsItemSelected(item);
@@ -73,26 +67,12 @@ public  class ForecastFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-//            flavia
-        String[] forecastArray = {
-                "Today - Sunny - 88/63",
-                "Tomorrow - Foggy - 70/40",
-                "Weds - Cloudy - 72/63",
-                "Thurs - Asteroids - 75/65",
-                "Fri - Heavy Rain - 65/56",
-                "Sat - HELP TRAPPED IN WEATHERSTATION - 60/51",
-                "Sun - Sunny - 80/68"
-        };
-
-        List<String> weekForescat = new ArrayList<String>(
-                Arrays.asList(forecastArray));
         mForecastAdapter =
                 new ArrayAdapter<String>(
                         getActivity(),
                         R.layout.list_item_forecast,
                         R.id.list_item_forecast_textview,
-                        weekForescat);
+                        new ArrayList<String>());
 
         ListView listview = (ListView) rootView.findViewById(R.id.listview_forecast);
         listview.setAdapter(mForecastAdapter);
@@ -106,6 +86,19 @@ public  class ForecastFragment extends Fragment {
         });
         return rootView;
     }
+        private void updateWeather() {
+                FetchWeatherTask weatherTask = new FetchWeatherTask();
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+                String location = prefs.getString(getString(R.string.pref_location_key),
+                                getString(R.string.pref_location_default));
+                weatherTask.execute(location);
+            }
+
+                @Override
+        public void onStart() {
+            super.onStart();
+            updateWeather();
+        }
 
     public class FetchWeatherTask extends AsyncTask <String, Void, String[]>{
 
