12f394227ac52c0f1a8bbdda81bc410489cf1bb6
Flávia Amaral <flavia.amaral@indexis.com.br>
2016-06-27 23:34:16 -0300

    add code add intentes map class 22 - 25 - lesson 3

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.android.sunshine.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,110 +43,36 @@ public class MainActivity extends ActionBarActivity {
             startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
-
+        if (id == R.id.action_map) {
+            openPreferredLocationInMap();
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-//    public static class ForecastFragment extends Fragment {
-////        flavia
-//        private ArrayAdapter<String> mForecastAdapter;
-//
-//        public ForecastFragment() {
-//        }
-//
-//        @Override
-//        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                                 Bundle savedInstanceState) {
-//            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-//
-////            flavia
-//            String[] forecastArray = {
-//                    "Today - Sunny - 88/63",
-//                    "Tomorrow - Foggy - 70/40",
-//                    "Weds - Cloudy - 72/63",
-//                    "Thurs - Asteroids - 75/65",
-//                    "Fri - Heavy Rain - 65/56",
-//                    "Sat - HELP TRAPPED IN WEATHERSTATION - 60/51",
-//                    "Sun - Sunny - 80/68"
-//            };
-//
-//            List<String> weekForescat = new ArrayList<String>(
-//                    Arrays.asList(forecastArray));
-//            mForecastAdapter =
-//                    new ArrayAdapter<String>(
-//                     getActivity(),
-//                            R.layout.list_item_forecast,
-//                            R.id.list_item_forecast_textview,
-//                            weekForescat);
-//
-//            ListView listview = (ListView) rootView.findViewById(R.id.listview_forecast);
-//            listview.setAdapter(mForecastAdapter);
-//
-//            // These two need to be declared outside the try/catch
-//// so that they can be closed in the finally block.
-//            HttpURLConnection urlConnection = null;
-//            BufferedReader reader = null;
-//
-//// Will contain the raw JSON response as a string.
-//            String forecastJsonStr = null;
-//
-//            try {
-//                // Construct the URL for the OpenWeatherMap query
-//                // Possible parameters are available at OWM's forecast API page, at
-//                // http://openweathermap.org/API#forecast
-//                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/city?id=3460064&APPID=ec812d031039a127c436906e5e158580&units=metric&cnt=7");
-//
-//                // Create the request to OpenWeatherMap, and open the connection
-//                urlConnection = (HttpURLConnection) url.openConnection();
-//                urlConnection.setRequestMethod("GET");
-//                urlConnection.connect();
-//
-//                // Read the input stream into a String
-//                InputStream inputStream = urlConnection.getInputStream();
-//                StringBuffer buffer = new StringBuffer();
-//                if (inputStream == null) {
-//                    // Nothing to do.
-//                    forecastJsonStr = null;
-//                }
-//                reader = new BufferedReader(new InputStreamReader(inputStream));
-//
-//                String line;
-//                while ((line = reader.readLine()) != null) {
-//                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-//                    // But it does make debugging a *lot* easier if you print out the completed
-//                    // buffer for debugging.
-//                    buffer.append(line + "\n");
-//                }
-//
-//                if (buffer.length() == 0) {
-//                    // Stream was empty.  No point in parsing.
-//                    forecastJsonStr = null;
-//                }
-//                forecastJsonStr = buffer.toString();
-//            } catch (IOException e) {
-//                Log.e("PlaceholderFragment", "Error ", e);
-//                // If the code didn't successfully get the weather data, there's no point in attempting
-//                // to parse it.
-//                forecastJsonStr = null;
-//            } finally{
-//                if (urlConnection != null) {
-//                    urlConnection.disconnect();
-//                }
-//                if (reader != null) {
-//                    try {
-//                        reader.close();
-//                    } catch (final IOException e) {
-//                        Log.e("PlaceholderFragment", "Error closing stream", e);
-//                    }
-//                }
-//            }
-//
-//
-//
-//            return rootView;
-//        }
-//    }
+    private void openPreferredLocationInMap() {
+                SharedPreferences sharedPrefs =
+                                PreferenceManager.getDefaultSharedPreferences(this);
+                String location = sharedPrefs.getString(
+                                getString(R.string.pref_location_key),
+                                getString(R.string.pref_location_default));
+
+                        // Using the URI scheme for showing a location found on a map.  This super-handy
+                                // intent can is detailed in the "Common Intents" page of Android's developer site:
+                                        // http://developer.android.com/guide/components/intents-common.html#Maps
+                                                Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                                .appendQueryParameter("q", location)
+                                .build();
+
+                        Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(geoLocation);
+
+                        if (intent.resolveActivity(getPackageManager()) != null) {
+                        startActivity(intent);
+                    } else {
+                        Log.d(LOG_TAG, "Couldn't call " + location + ", no receiving apps installed!");
+            }
+    }
+
+
 }
